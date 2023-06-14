d29d4388c7c6ab61a820a3407078a85f3a01bc86
Maria Gabriela Moreno Exposito <marimorexx@gmail.com>
2023-06-14 10:12:20 +0200

    use of qualifiers demo

---
+++ src/main/java/com/demo/springcoredemo/common/TennnisCoach.java
@@ -0,0 +1,12 @@
+package com.demo.springcoredemo.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TennnisCoach implements Coach{
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice your backhand volley";
+    }
+}
--- src/main/java/com/demo/springcoredemo/rest/DemoController.java
+++ src/main/java/com/demo/springcoredemo/rest/DemoController.java
@@ -2,6 +2,7 @@ package com.demo.springcoredemo.rest;
 
 import com.demo.springcoredemo.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,20 +14,20 @@ public class DemoController {
     /*
     CONSTRUCTOR INJECTION
     // define a constructor for dependency injection
+    */
     @Autowired
-    public DemoController(Coach theCoach){
+    public DemoController(@Qualifier("baseballCoach") Coach theCoach){
         myCoach = theCoach;
     }
-    */
 
     /*
     SETTER INJECTION
-    */
+
     @Autowired
     public void setCoach(Coach theCoach) {
         this.myCoach = theCoach;
     }
-
+    */
     @GetMapping("/dailyworkout")
     public String getDailyWorkout(){
         return myCoach.getDailyWorkout();
